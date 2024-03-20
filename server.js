99dfaa2fc5dbf53f3028ef07227157a7d9fc11e4
SolomonPipi <solomonpipi1@gmail.com>
2024-03-20 10:25:00 +0000

    fixed small typos in server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use(express.static("public"));
 app.use("/videos", videoRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
